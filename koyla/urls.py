583456ab243edb8f51aa65aff5fe491300207452
armikhalev <armikhalev@gmail.com>
2017-04-25 19:28:22 -0400

    Updating views and urls to make one letter query.

--- koyla/urls.py
+++ koyla/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-		url('^english/(?P<letter>.+)/$', views.WordSet.as_view()),
-		url('^mela/(?P<letter>.+)/$', views.LaSet.as_view()),
+		url('^words/$', views.WordSet.as_view()),
+		url('^las/$', views.LaSet.as_view()),
 ]
--- koyla/views.py
+++ koyla/views.py
@@ -7,16 +7,24 @@ from .serializers import KoylaSerializer
 class WordSet(generics.ListAPIView):
 
 	serializer_class = KoylaSerializer
-
+     
 	def get_queryset(self):
-		queryset = self.kwargs['letter']
-		return Koyla.objects.filter(word__startswith=queryset)
+		queryset = Koyla.objects.all()
+		letter = self.request.query_params.get('letter', None)
+		if letter is not None:
+			queryset = queryset.filter(word__startswith=letter)
+
+		return queryset
 
 # Mela words
 class LaSet(generics.ListAPIView):
 
 	serializer_class = KoylaSerializer
-
+     
 	def get_queryset(self):
-		queryset = self.kwargs['letter']
-		return Koyla.objects.filter(la__startswith=queryset)
+		queryset = Koyla.objects.all()
+		la = self.request.query_params.get('la', None)
+		if la is not None:
+			queryset = queryset.filter(la__startswith=la)
+
+		return queryset
