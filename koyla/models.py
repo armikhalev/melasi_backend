4032a22f10beb30769495c7a4b7fac9ed36b4ae1
armikhalev <armikhalev@gmail.com>
2017-06-14 21:57:30 -0400

    Adding grammarCard model and serializer. Fixing API to send nested cards with grammarCard json.

--- koyla/models.py
+++ koyla/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 
+class GrammarCard(models.Model):
+	title = models.CharField(max_length=100)
+	body = models.TextField()
+	comment = models.TextField()
+
+	def __str__(self):
+		return self.title + ", " + self.body  + ", " + self.comment
+
 class Koyla(models.Model):
 	word = models.CharField(max_length=30)
 	la = models.CharField(max_length=30)
 	comment = models.CharField(max_length=100)
+	grammarCard = models.ForeignKey(GrammarCard)
 
 	def __str__(self):
 		return self.word + " == " + self.la + ", comment: " + self.comment
@@ -12,6 +21,7 @@ class Card(models.Model):
 	front = models.CharField(max_length=100)
 	back = models.CharField(max_length=100)
 	flip = models.BooleanField()
+	grammarCard = models.ForeignKey(GrammarCard)
 
 	def __str__(self):
 		return self.front + " == " + self.back
@@ -19,11 +29,3 @@ class Card(models.Model):
 class Intro(models.Model):
 	title = models.CharField(max_length=100)
 	body = models.TextField()
-
-class GrammarCard(models.Model):
-	title = models.CharField(max_length=100)
-	body = models.TextField()
-	comment = models.TextField()
-
-	def __str__(self):
-		return self.title + ", " + self.body  + ", " + self.comment
--- koyla/serializers.py
+++ koyla/serializers.py
@@ -1,26 +1,29 @@
 from rest_framework import serializers
 from .models import Koyla, Card, Intro, GrammarCard
 
-class KoylaSerializer(serializers.HyperlinkedModelSerializer):
-	id = serializers.ReadOnlyField()
-	class Meta:
-		model = Koyla
-		fields = ('id','word','la','comment')
 
-class CardSerializer(serializers.HyperlinkedModelSerializer):
-	id = serializers.ReadOnlyField()
-	class Meta:
-		model = Card
-		fields = ('id','front','back','flip')
-
-class IntroSerializer(serializers.HyperlinkedModelSerializer):
-	id = serializers.ReadOnlyField()
-	class Meta:
-		model = Intro
-		fields = ('id','title','body')
-
-class GrammarCardSerializer(serializers.HyperlinkedModelSerializer):
+class GrammarCardSerializer(serializers.ModelSerializer):
 	id = serializers.ReadOnlyField()
 	class Meta:
 		model = GrammarCard
 		fields = ('id', 'title', 'body', 'comment')
+
+class KoylaSerializer(serializers.ModelSerializer):
+	id = serializers.ReadOnlyField()
+	grammarCard = GrammarCardSerializer(read_only=True)
+	class Meta:
+		model = Koyla
+		fields = ('id','word','la','comment','grammarCard')
+
+class CardSerializer(serializers.ModelSerializer):
+	id = serializers.ReadOnlyField()
+	grammarCard = GrammarCardSerializer(read_only=True)
+	class Meta:
+		model = Card
+		fields = ('id','front','back','flip','grammarCard')
+
+class IntroSerializer(serializers.ModelSerializer):
+	id = serializers.ReadOnlyField()
+	class Meta:
+		model = Intro
+		fields = ('id','title','body')
